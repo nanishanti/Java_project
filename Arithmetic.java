f08976a0b88c321b603f3d8b26b93ae33996e375
nanishanti <nanishanti@gmail.com>
2023-07-27 14:05:07 -0400

    java code comited

---
+++ Arithmetic.java
@@ -0,0 +1,27 @@
+public class Arithmetic
+{
+int i=10;
+int j=20;
+
+
+public void add()
+{
+int add=i+j;
+System.out.println("the addition of i and j::"+add);
+
+}
+
+public void sub()
+{
+int sub=i-j;
+System.out.println("the subtraction of i and j::"+sub);
+}
+
+public static void main(String args[])
+{
+Arithmetic obj=new Arithmetic();
+obj.add();
+obj.sub();
+}
+}
+
